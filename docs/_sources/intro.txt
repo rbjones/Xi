a35952f519b79ed46518d756bb4607fefece1d97
Roger Bishop Jones <rbj@rbjones.com>
2016-10-03 22:19:03 +0100

    More documentation preliminaries

--- docs/_sources/intro.txt
+++ docs/_sources/intro.txt
@@ -34,3 +34,12 @@ which I think of greatest interest.
 
    Work with the Xi ITP starts in an existing "context" which is defined by a file
 
+1. It will support the abstract logic &XiL;, which has no definite concrete syntax.  Other languages and logics as concrete presentations of &Xi;L expressions.
+
+2. The logic will be a Hilbert style deductive system.
+
+3. The implementqtion paradigm will be derived from the LCF paradigm.
+
+4. The implementation language will be python, but an executable subset of &Xi; will be used for tactical programming.
+
+5. Theorem proving will be *distributed*.
--- src/Xip.py
+++ src/Xip.py
@@ -1,11 +1,11 @@
-# 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    The Xi Interactive Theorem Prover 
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-1. It will support the abstract logic &XiL;, which has no definite concrete syntax.  Other languages and logics as concrete presentations of &Xi;L expressions.
+    :copyright: Copyright 2016 Roger Bishop Jones
+    :license: BSD, see LICENSE for details.
+"""
 
-2. The logic will be a Hilbert style deductive system.
-
-3. The implementqtion paradigm will be derived from the LCF paradigm.
-
-4. The implementation language will be python, but an executable subset of &Xi; will be used for tactical programming.
-
-5. Theorem proving will be *distributed*.
+if __name__ == '__main__':
